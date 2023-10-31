cedec0b81412f68bc73ce22704d1e698a12f8e4e
Bartosz Dobija <33185107+BartShoot@users.noreply.github.com>
2023-10-31 12:37:37 +0100

    better seleect image

--- PrototypeWPF/PrototypeWPF/Operations/SelectImage.cs
+++ PrototypeWPF/PrototypeWPF/Operations/SelectImage.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Win32;
 using OpenCvSharp;
+using PrototypeWPF.OperationsViews;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -10,23 +11,11 @@ using System.Windows.Controls;
 
 namespace PrototypeWPF.Operations
 {
-    internal class SelectImage : IOperation
+    public class SelectImage : IOperation
     {
         private Mat _input;
         private Mat _output;
         private string _path;
-
-        public Mat Input
-        {
-            get { return _input; }
-            set => _input = value;
-        }
-
-        public Mat Output
-        {
-            get { return _output; }
-            set => _output = value;
-        }
 
         public string Name => "Select image";
 
@@ -39,16 +28,33 @@ namespace PrototypeWPF.Operations
             op.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
                         "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
                         "Portable Network Graphic (*.png)|*.png";
-            if (op.ShowDialog() == true)
+            if (Path != null)
             {
-                _path = op.FileName;
+                _input = new Mat(Path, ImreadModes.Unchanged);
+            }
+            else if (op.ShowDialog() == true)
+            {
+                Path = op.FileName;
                 _input = new Mat(op.FileName, ImreadModes.Unchanged);
             }
             Output = Input;
             return Output;
         };
 
-        public UserControl ParametersView => new UserControl();
+        public UserControl ParametersView => new SelectImageView(this);
+
+        public string Path { get => _path; set => _path = value; }
+        public Mat Input
+        {
+            get { return _input; }
+            set => _input = value;
+        }
+
+        public Mat Output
+        {
+            get { return _output; }
+            set => _output = value;
+        }
 
     }
 }
---
+++ PrototypeWPF/PrototypeWPF/OperationsViews/SelectImageView.xaml.cs
@@ -0,0 +1,38 @@
+﻿using OpenCvSharp;
+using PrototypeWPF.Operations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PrototypeWPF.OperationsViews
+{
+    public partial class SelectImageView : UserControl
+    {
+        SelectImage selectImage;
+        public SelectImageView(SelectImage function)
+        {
+            selectImage = function;
+            InitializeComponent();
+            if(selectImage.Path != null)
+            {
+                FilePath.Text = selectImage.Path;
+            }
+        }
+
+        private void SaveChanges(object sender, RoutedEventArgs e)
+        {
+            selectImage.Path = FilePath.Text;
+        }
+    }
+}
