1a2ece19560edad4733382ef10ce0a79883af7d3
Bartosz Dobija <33185107+BartShoot@users.noreply.github.com>
2023-11-15 12:26:43 +0100

    Better Save operation

---
+++ PrototypeWPF/PrototypeWPF/OperationsViews/SaveImageView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using PrototypeWPF.Operations;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PrototypeWPF.OperationsViews
+{
+    /// <summary>
+    /// Interaction logic for SaveImageView.xaml
+    /// </summary>
+    public partial class SaveImageView : UserControl
+    {
+        SaveImage saveImage;
+
+        public SaveImageView(SaveImage saveImage)
+        {
+            this.saveImage = saveImage;
+            InitializeComponent();
+            if (saveImage.Path != null)
+            {
+                FilePath.Text = saveImage.Path;
+            }
+
+        }
+
+        private void SaveChanges(object sender, RoutedEventArgs e)
+        {
+            saveImage.Path = FilePath.Text;
+        }
+
+    }
+}
