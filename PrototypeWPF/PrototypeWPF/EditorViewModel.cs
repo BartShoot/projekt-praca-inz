cd185f065970660d64612062bc81433e304bb0e5
Bartosz Dobija <33185107+BartShoot@users.noreply.github.com>
2023-11-24 19:51:42 +0100

    initial implementation

--- PrototypeWPF/PrototypeWPF/EditorViewModel.cs
+++ PrototypeWPF/PrototypeWPF/EditorViewModel.cs
@@ -8,13 +8,40 @@ using System.Threading.Tasks;
 
 namespace PrototypeWPF
 {
+    public class ConnectorViewModel
+    {
+        public string Title { get; set; }
+    }
+    public class NodeViewModel
+    {
+        public string Title { get; set; }
+        public ObservableCollection<ConnectorViewModel> Input { get; set; } = new ObservableCollection<ConnectorViewModel>();
+        public ObservableCollection<ConnectorViewModel> Output { get; set; } = new ObservableCollection<ConnectorViewModel>();
+    }
     public class EditorViewModel
     {
-        public ObservableCollection<IOperation> Nodes { get; } = new ObservableCollection<IOperation> ();
+        public ObservableCollection<NodeViewModel> Nodes { get; } = new ObservableCollection<NodeViewModel> ();
 
         public EditorViewModel()
         {
-            Nodes.Add(new Blur());
+            Nodes.Add(new NodeViewModel
+            {
+                Title = "Welcome",
+                Input = new ObservableCollection<ConnectorViewModel>
+                {
+                    new ConnectorViewModel
+                    {
+                        Title = "In"
+                    }
+                },
+                Output = new ObservableCollection<ConnectorViewModel>
+                {
+                    new ConnectorViewModel
+                    {
+                        Title = "Out"
+                    }
+                }
+            });
         }
     }
 }
