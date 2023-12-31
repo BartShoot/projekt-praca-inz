9e545ea678b2368acbaeed0bb52bf6c4a6af4305
Bartosz Dobija <33185107+BartShoot@users.noreply.github.com>
2023-12-31 01:14:25 +0100

    Not working, binding not updating

---
+++ PrototypeWPF/PrototypeWPF/Utilities/NodeViewModelToImageSourceConverter.cs
@@ -0,0 +1,25 @@
+﻿using OpenCvSharp;
+using PrototypeWPF.ViewModels.Editor;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace PrototypeWPF.Utilities;
+
+internal class NodeViewModelToImageSourceConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value != null)
+        {
+            var ViewModel = (NodeViewModel)value;
+            return BitmapExtensions.ToBitmapSourceBGR(OpenCvSharp.Extensions.BitmapConverter.ToBitmap(ViewModel.Output[0].Data.Get<Mat>()));
+        }
+        return null;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
