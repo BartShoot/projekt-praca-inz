a9d9ba58686c5d917901b28c10187ac8f179d326
Lukas <lucas.hamera@gmail.com>
2024-01-14 17:48:56 +0100

    Added validation for image path in save image operation

--- PrototypeWPF/NoodleCV.OpenCvSharp4/Operations/SaveImageValidator.cs
+++ PrototypeWPF/NoodleCV.OpenCvSharp4/Operations/SaveImageValidator.cs
@@ -8,5 +8,6 @@ public class SaveImageValidator : AbstractValidator<IReadOnlyList<OperationInput
     public SaveImageValidator()
     {
         RuleFor(list => list[0].Get<Mat>()).NotNull().WithMessage("No image input");
+        RuleFor(list => list[1].Get<string>()).NotNull().NotEmpty().WithMessage("Empty image path.");
     }
 }
